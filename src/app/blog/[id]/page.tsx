ef4f19343a2b62fdb66c36ace5d9f5cf1fb35358
LeonV <leonardov0529@gmail.com>
2024-02-14 13:35:13 -0800

    Rough-in basic blog functionality

---
+++ src/app/blog/[id]/page.tsx
@@ -0,0 +1,37 @@
+import { getAllPostIds, getPostData } from '@/lib/posts';
+
+type Params = {
+  id: string;
+};
+
+type Props = {
+  params: Params;
+};
+
+type PostData = {
+  title: string;
+  date: string;
+  tags?: string[];
+  contentHtml: string;
+};
+
+export async function generateMetadata({ params }: Props) {
+  const postData: PostData = await getPostData(params.id);
+
+  return {
+    title: postData.title,
+  };
+}
+
+export default async function Post({ params }: Props) {
+  const postData: PostData = await getPostData(params.id);
+  const tags = postData.tags?.join(', ');
+  return (
+    <div>
+      <h1>{postData.title}</h1>
+      <p>{postData.date}</p>
+      <p>{tags}</p>
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </div>
+  );
+}
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+
+import { getSortedPostsData } from '@/lib/posts';
+
+type AllPostsData = {
+  date: string;
+  title: string;
+  id: string;
+  tags: string[];
+}[];
+
+export default function Blog() {
+  const allPostsData: AllPostsData = getSortedPostsData();
+  return (
+    <ul>
+      {allPostsData.map(({ id, date, title, tags }) => {
+        const tagsList = tags?.join(', ');
+
+        return (
+          <li key={id}>
+            <Link href={`/blog/${id}`}>{title}</Link>
+            <p>{date}</p>
+            <p>{tagsList}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
