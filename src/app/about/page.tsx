a3c50f469b4105bb69c6697e95619ccf76c4bcb1
LeonV <leonardov0529@gmail.com>
2024-02-13 19:25:25 -0800

    Configure site to use mdx

--- src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <main>
-      <div>
-        <h1>About</h1>
-      </div>
-    </main>
-  );
-}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import React from 'react';
 import { usePathname } from 'next/navigation';
 
 import { Page } from '../components/Page/Page';
