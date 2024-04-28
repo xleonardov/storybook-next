d39d1d39636f4b9b30281bf8b0389f10e1d5be86
LeonV <leonardov0529@gmail.com>
2024-04-27 17:20:00 -0700

    begin to redo colors

--- src/components/MainMenu/MainMenu.stories.tsx
+++ src/components/MainMenu/MainMenu.stories.tsx
@@ -3,44 +3,37 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { MainMenu } from './MainMenu';
 
 const meta = {
-  title: 'Components/Main Menu',
-  component: MainMenu,
-  tags: ['autodocs'],
-  parameters: { layout: 'fullscreen' },
-  argTypes: {},
-  args: {},
+	title: 'Components/Main Menu',
+	component: MainMenu,
+	tags: ['autodocs'],
+	parameters: { layout: 'fullscreen' },
+	argTypes: {},
+	args: {},
 } satisfies Meta<typeof MainMenu>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {
-    menuItems: [
-      {
-        label: 'About',
-        url: '/about',
-      },
-      {
-        label: 'Portfolio',
-        url: '/portfolio',
-      },
-      {
-        label: 'Blog',
-        url: '/blog',
-      },
-      {
-        label: 'Contact',
-        url: '/contact',
-      },
-    ],
-    pathname: '/portfolio/bux',
-  },
-  decorators: [
-    (Story) => (
-      <div style={{ backgroundColor: 'var(--mm-color-neutral-darkest)' }}>
-        <Story />
-      </div>
-    ),
-  ],
+	args: {
+		menuItems: [
+			{
+				label: 'About',
+				url: '/about',
+			},
+			{
+				label: 'Portfolio',
+				url: '/portfolio',
+			},
+			{
+				label: 'Blog',
+				url: '/blog',
+			},
+			{
+				label: 'Contact',
+				url: '/contact',
+			},
+		],
+		pathname: '/portfolio/bux',
+	},
 };
