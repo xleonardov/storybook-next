59a2db79fbd05d19103c4a20d8cf9e5f2aaed9b0
LeonV <leonardov0529@gmail.com>
2024-04-30 11:27:01 -0700

    Make site name active color on home page

--- src/app/HomeContent.tsx
+++ src/app/HomeContent.tsx
@@ -1,22 +1,19 @@
 'use client';
 
 import React from 'react';
-import { usePathname } from 'next/navigation';
 
 import { Hero } from '@/components/Hero/Hero';
 import { Page as PageLayout } from '../layout-components/Page/Page';
 
 import { heroData } from '@/data/about-data';
 
-export interface HomeContentProps {
-	pageTitle?: string;
-}
-
-export default function HomeContent({ pageTitle }: HomeContentProps) {
-	const pathname = usePathname();
-
+export default function HomeContent() {
 	return (
-		<PageLayout containerWidth='standard' showBreadcrumb={false}>
+		<PageLayout
+			containerWidth='standard'
+			showBreadcrumb={false}
+			className='mm-site-home'
+		>
 			<Hero
 				headline={heroData.headline}
 				description={heroData.description}
